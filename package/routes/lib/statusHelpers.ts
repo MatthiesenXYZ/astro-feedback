b453b43461e276f470dc6e57161a6dc8db0efa9d
Adam Matthiesen <amatthiesen@outlook.com>
2024-09-19 07:26:36 -0700

    update

--- package/routes/lib/statusHelpers.ts
+++ package/routes/lib/statusHelpers.ts
@@ -1,8 +1,20 @@
-export function convertStatusString(str: string): string {
-	return str
-		.split('-')
-		.map((w) => w.charAt(0).toUpperCase() + w.slice(1))
-		.join(' ');
+import { type ui, useTranslations } from '../i18n';
+
+export function convertStatusString(str: string, lang: keyof typeof ui): string {
+	const t = useTranslations(lang, 'components/open-submission-status');
+
+	switch (str) {
+		case 'open':
+			return t('open');
+		case 'in-progress':
+			return t('in-progress');
+		case 'closed':
+			return t('closed');
+		case 'resolved':
+			return t('resolved');
+		default:
+			return '';
+	}
 }
 
 export function getSubmissionStatusColor(status: string): string | undefined {
